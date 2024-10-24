976dbf5c176c46a34bf93cf38b715793c9ffb880
yuapX <shesterovmm@gmail.com>
2024-10-24 19:42:17 +0300

    sd

--- frontend/src/Components/CategoryCard/CategoryCard.jsx
+++ frontend/src/Components/CategoryCard/CategoryCard.jsx
@@ -7,25 +7,27 @@ function CategoryCard({item}) {
     const navigate = useNavigate();
     const {
         id,
+        game_id,
         name,
-        game,
-        image_url
+        image,
+        is_visible,
+        thread_id
     } = item;
 
     return <div onClick={() => {
         navigate(`/category?id=${id}`)
     }} className='card horizontal-padding' >
         <div className="image_container">
-            <img className='card__image' src={image_url} alt={name}/>
+            <img className='card__image' src={image} alt={name}/>
         </div>
         <div className="card__title">
             <h4>
                 {name}
             </h4>
         </div>
-        <div className="card__title">
+        {/* <div className="card__title">
             <small>{game}</small>
-        </div>
+        </div> */}
     </div>
 }
 
