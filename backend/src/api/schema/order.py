86b7ac1c458f9a284f1ef3f3488a4ebf200db573
yuapX <shesterovmm@gmail.com>
2024-10-27 14:48:37 +0300

    sd

--- backend/src/api/schema/order.py
+++ backend/src/api/schema/order.py
@@ -1,42 +1,8 @@
 from uuid import UUID
-from typing import Optional, Union, Any, TypeAlias
 
-from pydantic import BaseModel, EmailStr, Field
-
-
-class BaseAdditionalData(BaseModel):
-    login: Any
-    password: Any
-
-
-class SupercellData(BaseModel):
-    email: EmailStr
-    code: int = Field(le=999_999, ge=100_000)
-
-
-class RobloxData(BaseModel):
-    login: str
-    password: int
-    two_factor_code: Optional[int]
-
-
-class StumbleGuysData(BaseModel):
-    nickname: str
-
-
-class PubgData(BaseModel):
-    pubg_id: str
-
-
-AdditionalData: TypeAlias = Union[
-    BaseAdditionalData,
-    SupercellData,
-    RobloxData,
-    StumbleGuysData,
-    PubgData,
-]
+from pydantic import BaseModel
 
 
 class CreateOrderDTO(BaseModel):
     product_id: UUID
-    additional_data: AdditionalData
+    additional_data: dict
--- backend/src/utils/json_text_getter.py
+++ backend/src/utils/json_text_getter.py
@@ -28,57 +28,17 @@ def get_order_info_text(
     product: Product,
     category: str,
 ) -> Optional[str]:
-    if product.game_name in ('Clash of Clans', 'Clash Royale', 'Brawl Stars', 'Squad Busters'):
-        return get_json_text('supercell_order').format(
-            order_id=order_id,
-            user_id=user_id,
-            game=product.game_name,
-            product_name=product.name,
-            product_price=product.price,
-            email=order_data.additional_data.email if not isinstance(order_data, dict) else order_data.get('email'),
-            code=order_data.additional_data.code if not isinstance(order_data, dict) else order_data.get('code'),
-            category=category,
-        )
-    elif product.game_name == 'Roblox':
-        return get_json_text('roblox_order').format(
-            order_id=order_id,
-            user_id=user_id,
-            game=product.game_name,
-            product_name=product.name,
-            product_price=product.price,
-            email=order_data.additional_data.login,
-            password=order_data.additional_data.password if not isinstance(order_data, dict) else order_data.get('password'),
-            two_factor_code=order_data.additional_data.two_factor_code if not isinstance(order_data, dict) else order_data.get('two_factor_code'),
-            category=category,
-        )
-    elif product.game_name == 'PUBG':
-        return get_json_text('pubg_order').format(
-            order_id=order_id,
-            user_id=user_id,
-            game=product.game_name,
-            product_name=product.name,
-            product_price=product.price,
-            pubg_id=order_data.additional_data.pubg_id if not isinstance(order_data, dict) else order_data.get('pubg_id'),
-            category=category,
-        )
-    elif product.game_name == 'Stumble Guys':
-        return get_json_text('stumble_guys_order').format(
-            order_id=order_id,
-            user_id=user_id,
-            game=product.game_name,
-            product_name=product.name,
-            product_price=product.price,
-            nickname=order_data.additional_data.nickname if not isinstance(order_data, dict) else order_data.get('nickname'),
-            category=category,
-        )
-    else:
-        return get_json_text('base_order').format(
-            order_id=order_id,
-            user_id=user_id,
-            game=product.game_name,
-            product_name=product.name,
-            product_price=product.price,
-            login=order_data.additional_data.login if not isinstance(order_data, dict) else order_data.get('login'),
-            password=order_data.additional_data.password if not isinstance(order_data, dict) else order_data.get('password'),
-            category=category,
-        )
+    order_text = get_json_text('order_text').format(
+        order_id=order_id,
+        user_id=user_id,
+        game=product.game_name,
+        category=category,
+        product_name=product.name,
+        product_price=product.price
+    )
+
+    additional_data_text = ""
+    for key, value in order_data.additional_data.items():
+        additional_data_text += f"\n\n<b>{key}</b>: <code>{value}</code>"
+
+    return order_text + additional_data_text
