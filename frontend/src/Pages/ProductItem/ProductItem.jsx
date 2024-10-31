6a65dcf1bce12f70088f4e1b409df144b7e64690
yuapX <shesterovmm@gmail.com>
2024-10-31 21:49:04 +0200

    ыв

--- frontend/src/Pages/ProductItem/ProductItem.jsx
+++ frontend/src/Pages/ProductItem/ProductItem.jsx
@@ -75,15 +75,7 @@ function ProductItem() {
     const renderDescription = (text) => {
         const urlRegex = /(https?:\/\/[^\s]+)/g;
         return text.split('\n').map((paragraph, paragraphIndex) => (
-            <p key={paragraphIndex}>
-                {paragraph.split(urlRegex).map((part, partIndex) => 
-                    urlRegex.test(part) ? (
-                        <a key={partIndex} href={part} target="_blank" rel="noopener noreferrer">{part}</a>
-                    ) : (
-                        part
-                    )
-                )}
-            </p>
+            <p key={paragraphIndex}>{paragraph.split(urlRegex).map((part, partIndex) => urlRegex.test(part) ? (<a key={partIndex} href={part} target="_blank" rel="noopener noreferrer">{part}</a>) : (part))}</p>
         ));
     };
 
