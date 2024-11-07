f1775e17c5d5da95ad20f734f35191d7c0919b8d
yuapX <shesterovmm@gmail.com>
2024-11-07 17:43:49 +0200

    ыфы

--- bot/app/services/freekassa_service.py
+++ bot/app/services/freekassa_service.py
@@ -2,10 +2,10 @@ import requests
 import enum
 import hashlib
 import hmac
+import time
 from typing import Optional, Dict
 
 from app.main.config import settings
-
 
 class PaymentMethod(enum.Enum):
     CARD = 36
@@ -14,18 +14,15 @@ class PaymentMethod(enum.Enum):
 
 class FreeKassaService:
     API_URL = 'https://api.freekassa.com/v1/'
-    SHOP_ID = settings.FREEKASSA_SHOP_ID
-    SECRET_KEY = settings.FREEKASSA_SECRET_KEY
 
-    def __init__(self, shop_id: str, secret_key: str) -> None:
-        self.shop_id = shop_id
-        self.secret_key = secret_key
+    def __init__(self) -> None:
+        self.shop_id = settings.FREEKASSA_SHOP_ID
+        self.secret_key = settings.FREEKASSA_SECRET_KEY
 
     def create_order(
         self,
         amount: float,
         ip: str,
-        order_id: str,
         email: str,
         payment_method: PaymentMethod,
     ) -> Optional[Dict]:
@@ -35,7 +32,7 @@ class FreeKassaService:
             'amount': amount,
             'currency': 'RUB',
             'ip': ip,
-            'nonce': order_id,
+            'nonce': self._generate_nonce(),
             'email': email,
             'i': payment_method.value,
         }
@@ -52,5 +49,10 @@ class FreeKassaService:
         sorted_params = {key: params[key] for key in sorted(params.keys())}
         sign_string = '|'.join([str(value) for value in sorted_params.values()])
         signature = hmac.new(self.secret_key.encode('utf-8'), sign_string.encode('utf-8'), hashlib.sha256).hexdigest()
-        
+
         return signature
+
+    def _generate_nonce(self) -> int:
+        current_time = time.time()
+        random_number = int((current_time - int(current_time)) * 1000000)
+        return random_number
