a4f42065900d8516c5d2610fd1e819e8f9231b53
yuapX <shesterovmm@gmail.com>
2024-11-14 17:19:27 +0200

    ssd

--- backend/src/api/schema/payment.py
+++ backend/src/api/schema/payment.py
@@ -1,9 +1,13 @@
 from enum import Enum
 
 from pydantic import BaseModel, Field
-from src.services.freekassa_service import PaymentMethod
+
+
+class TopUpMethod(Enum):
+    SBP = 'sbp'
+    CARD = 'card'
 
 
 class TopUpSchema(BaseModel):
     amount: int = Field(ge=10, le=50000)
-    method: PaymentMethod = Field(default=PaymentMethod.CARD)
+    method: TopUpMethod = Field(default=TopUpMethod.CARD)
