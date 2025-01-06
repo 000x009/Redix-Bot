bfafee132e16ef16c7f0403bd312fc14e64e9294
yuapX <shesterovmm@gmail.com>
2025-01-06 15:45:16 +0200

    sd

--- backend/src/utils/json_text_getter.py
+++ backend/src/utils/json_text_getter.py
@@ -25,9 +25,9 @@ def get_order_info_text(
     game_name = product.game_name.strip()
     print(game_name == 'Clash Royale')
     if game_name == 'Clash Royale':
-        game_name == 'Clash of Clans'
+        game_name = 'Clash of Clans'
     if game_name == 'Clash of Clans':
-        game_name == 'Clash Royale'
+        game_name = 'Clash Royale'
 
     print("GAME NAME NEW", game_name)
 
@@ -40,10 +40,10 @@ def get_order_info_text(
         product_price=product.price
     )
 
-    if product.game_name == 'Clash of Clans':
-        order_text.replace('Clash of Clans', 'Clash Royale')
-    if product.game_name == 'Clash Royale':
-        order_text.replace('Clash Royale', 'Clash of Clans')
+    # if product.game_name == 'Clash of Clans':
+    #     order_text.replace('Clash of Clans', 'Clash Royale')
+    # if product.game_name == 'Clash Royale':
+    #     order_text.replace('Clash Royale', 'Clash of Clans')
     
     print(order_text)
 
