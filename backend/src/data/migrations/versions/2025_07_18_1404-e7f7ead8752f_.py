e2c689ff37c7bdf0c2ff88b7122ad64936446da6
yuapX <shesterovmm@gmail.com>
2025-07-20 12:35:21 +0300

    sdsd

--- backend/src/data/migrations/versions/2025_07_18_1404-e7f7ead8752f_.py
+++ backend/src/data/migrations/versions/2025_07_18_1404-e7f7ead8752f_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'e7f7ead8752f'
-down_revision: Union[str, None] = 'a13138fb311d'
+down_revision: Union[str, None] = '1bf6e34f2171'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
