478a9b9669b9937eb09479e5188db88e0201d4cb
yuapX <shesterovmm@gmail.com>
2025-07-20 13:06:05 +0300

    ssd

--- frontend/src/db/db.jsx
+++ frontend/src/db/db.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const API_URL = "https://redixshop.com/api";
-const API_URL = "http://localhost:8000";
+const API_URL = "https://redixshop.com/api";
 
 export function getXPadding() {
     return "1.5rem";
