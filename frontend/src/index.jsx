33753a2da445fb2d52e799a4432206d22f18018f
yuapX <shesterovmm@gmail.com>
2025-07-20 12:42:04 +0300

    fix build

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { init } from './init';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-await init()
 
 root.render(
   <React.StrictMode>
