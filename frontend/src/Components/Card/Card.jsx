f9aec09eda7eeaa64f37ab64a96c06767a581632
yuapX <shesterovmm@gmail.com>
2025-07-26 18:20:03 +0300

    ывыв

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -6,12 +6,23 @@ import {useNavigate} from "react-router-dom";
 function Card({item}) {
     const navigate = useNavigate();
 
-    return <div onClick={() => {
-        if (item.name === "Telegram Stars") {
+    if (item.name === "Telegram Stars") {
+        return <div onClick={() => {
             navigate('/telegram-stars', {state: {categoryId: item.id}});
-        } else {
-            navigate(`/product/${item.id}`)
-        }
+        }} className='card horizontal-padding' >
+            <div className="image_container">
+                <img className='card__image' src={item.image_url} alt={item.name}/>
+            </div>
+            <div className="card__title">
+                <h4>
+                    {item.name}
+                </h4>
+            </div>
+        </div>
+    }
+
+    return <div onClick={() => {
+        navigate(`/product/${item.id}`)
     }} className='card horizontal-padding' >
         <div className="image_container">
             <img className='card__image' src={item.image_url} alt={item.name}/>
